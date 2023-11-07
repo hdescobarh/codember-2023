4c932d775d745d8524ba3ed2ad2e3b716af13d4b
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-11-07 06:59:27 -0500

    feat(challenge01): solved the challenge. Pending Secrets...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod challenge01;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // Challenge_01
+    let message = fs::read_to_string("./data/message_01.txt").expect("Failed to read the file");
+    println!("{}", challenge01::decoder(&message))
 }
