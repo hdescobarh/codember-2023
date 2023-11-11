d28c450196667246eff3e5746fe98c3aa9e0d0d1
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-11-10 23:15:32 -0500

    feat(challenge02): solved correctly the challenge

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod challenge01;
+mod challenge02;
 use std::fs;
 
 fn main() {
-    // Challenge_01
+    // Challenge 01 — Encrypted message
+    println!("Solving Challenge 01...");
     let message = fs::read_to_string("./data/message_01.txt").expect("Failed to read the file");
-    println!("{}", challenge01::decoder(&message))
+    println!("{}\n", challenge01::decoder(&message));
+
+    // Challenge 02 — Mini compiler
+    println!("Solving Challenge 02...");
+    let message = fs::read_to_string("./data/message_02.txt").expect("Failed to read the file");
+    let mut compiler = challenge02::Compiler::new();
+    println!("{}", compiler.build(&message));
 }
