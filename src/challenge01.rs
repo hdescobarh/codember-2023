b655012d83c5f6ea58c0c7fa668ebbf1d9b442bd
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-11-10 21:51:10 -0500

    doc(challenge01): added challenge description

--- src/challenge01.rs
+++ src/challenge01.rs
@@ -1,3 +1,18 @@
+/*
+A spy is sending encrypted messages.
+
+Your mission is to create a program that decodes the messages.
+
+The messages are words separated by spaces like this:
+cat dog dog car Cat doG sun
+
+We need the program to return the number of times each word appears in the message, regardless of whether it is in uppercase or lowercase.
+
+The result will be a text string with the word and the number of times it appears in the message, in this format:
+cat2dog3car1sun1
+
+The words are sorted by their first appearance in the message!
+*/
 use std::collections::HashMap;
 
 pub fn decoder(message: &str) -> String {
