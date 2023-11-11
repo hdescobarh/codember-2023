7a6463cccd981e9241f8c06abf45dbaa6f747c35
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-11-10 23:10:43 -0500

    chore(challenge02): init library

---
+++ src/challenge02.rs
@@ -0,0 +1,18 @@
+/*
+** Mini Compiler Challenge **
+In the world of espionage, a coding language is used with symbols that perform simple mathematical operations.
+
+Your task is to create a mini compiler that interprets and executes programs written in this symbol language.
+
+The operations of the language are as follows:
+
+"#" Increases the numeric value by 1.
+"@" Decreases the numeric value by 1.
+"*" Multiplies the numeric value by itself.
+"&" Prints the current numeric value.
+The initial numeric value is 0 and the operations should be applied in the order in which they appear in the string of symbols.
+
+
+** Your Challenge: **
+Develop a mini compiler that takes a text string and returns another text string with the result of executing the program.
+*/
