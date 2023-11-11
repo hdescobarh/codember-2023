2acf47dd66e9027a56ffc895bd819bddfcb63a6c
Hans D. Escobar H <hdescobarh@unal.edu.co>
2023-11-10 23:11:37 -0500

    feat(challenge02): now can compile

--- src/challenge02.rs
+++ src/challenge02.rs
@@ -16,3 +16,56 @@ The initial numeric value is 0 and the operations should be applied in the order
 ** Your Challenge: **
 Develop a mini compiler that takes a text string and returns another text string with the result of executing the program.
 */
+
+pub struct Compiler {
+    value: isize,
+    output: String,
+}
+
+impl Compiler {
+    pub fn new() -> Self {
+        Self {
+            value: 0,
+            output: String::new(),
+        }
+    }
+    pub fn build(&mut self, token_stream: &str) -> &str {
+        for token in token_stream.chars() {
+            self.map_symbols(token);
+        }
+        &self.output
+    }
+
+    fn map_symbols(&mut self, instruction: char) {
+        match instruction {
+            '#' => self.value += 1,
+            '@' => self.value -= 1,
+            '*' => self.value = self.value.pow(2),
+            '&' => self.output.push_str(&self.value.to_string()),
+            _ => panic!("Invalid instruction."),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    /*
+    ** Input Examples: **
+    Input: "##*&"
+    Expected Output: "4"
+    Explanation: Increment (1), increment (2), multiply (4), print (4).
+
+    Input: "&##&*&@&"
+    Expected Output: "0243"
+    Explanation: Print (0), increment (1), increment (2), print (2), multiply (4), print (4), decrement (3), print (3).
+     */
+
+    #[test]
+    fn simple_input() {
+        let mut compiler = Compiler::new();
+        assert_eq!("4", compiler.build("##*&"));
+        let mut compiler = Compiler::new();
+        assert_eq!("0243", compiler.build("&##&*&@&"));
+    }
+}
